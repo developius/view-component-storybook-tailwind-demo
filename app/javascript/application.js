eb3cf7006ed24150c4f5f7008231f61c0900f7de
Finnian Anderson <get@finnian.io>
2021-08-29 13:36:19 +1200

    Initial Rails edge install

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb
+
+// import "@rails/actioncable"
+// import "@rails/activestorage"
+import "@hotwired/turbo-rails"
+
+import "@hotwired/stimulus-importmap-autoloader"
